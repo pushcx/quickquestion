0f245cdfc72e88b35f710dae28d389ed8c2f737d
Peter Bhat Harkins <peter@push.cx>
2024-08-11 21:05:43 -0500

    version the log db

--- lib/qq.rb
+++ lib/qq.rb
@@ -4,22 +4,45 @@ require "fileutils"
 require "xdg"
 
 module QQ
+  VERSION = "0.1.0" # Add this line to define the current version
   DB_PATH = File.join(XDG::Config.new.home, "qq/log.sqlite3")
 
   def self.setup_db
     FileUtils.mkdir_p(File.dirname(DB_PATH))
     @db ||= Sequel.connect("sqlite://#{DB_PATH}")
+
     @db.create_table? :queries do
       primary_key :id
-      DateTime :at
-      String :query
-      String :response
+      DateTime :at, null: false
+      String :query, null: false
+      String :response, null: false
     end
+
+    @db.create_table? :version do
+      String :version, null: false
+    end
+
+    check_and_update_version
     @db
   end
 
   def self.db
     @db ||= setup_db
+  end
+
+  def self.check_and_update_version
+    version_count = @db[:version].count
+
+    if version_count == 0
+      @db[:version].insert(version: VERSION)
+    elsif version_count > 1
+      raise "Invalid 'version' table: multiple rows found"
+    else
+      db_version = @db[:version].first[:version]
+      if db_version != VERSION
+        warn "Warning: Database version (#{db_version}) does not match current version (#{VERSION})"
+      end
+    end
   end
 
   def self.query(input)
